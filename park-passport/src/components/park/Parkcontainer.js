86b63ea3e7fc72c04483d340cb2b801b5248b93b
Alison Law <alisparklaw@gmail.com>
2019-11-17 23:16:51 +0000

    park input flex-box

--- park-passport/src/components/park/Parkcontainer.js
+++ park-passport/src/components/park/Parkcontainer.js
@@ -2,6 +2,18 @@ import React, { useState } from "react";
 
 import ParkForm from "./ParkForm";
 import ParksList from "./ParksList";
+import styled from "styled-components";
+
+// Styling
+const StyledDiv = styled.div`
+  width: 61.8%;
+  margin: 0 auto;
+  /* border: 1px solid blue; */
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3);
+`;
 
 export default function Parks() {
   const [parks, setParks] = useState([
@@ -16,18 +28,18 @@ export default function Parks() {
   const addNewPark = park => {
     const newPark = {
       ...park,
-      id: Date.now()
+      id: Date.now();
     };
 
     setParks([...parks, newPark]);
   };
 
   return (
-    <div className="Parks">
+    <StyledDiv className="Parks">
       <h1>Parks</h1>
       {/* we are going to pass a function down as a prop */}
       <ParkForm addNewPark={addNewPark} />
       <ParksList parks={parks} />
-    </div>
+    </StyledDiv>
   );
 }
